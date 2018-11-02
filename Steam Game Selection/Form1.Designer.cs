f2b41a527beef97ef4e7a658bf5152652c773271
SWH <almachino@yandex.ru>
2018-11-02 23:10:51 +0500

    Parsing, showing game,avg,mdm added

--- Steam Game Selection/Form1.Designer.cs
+++ Steam Game Selection/Form1.Designer.cs
@@ -31,8 +31,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,7 +58,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 64);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(444, 340);
@@ -62,13 +66,13 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Game";
             // 
-            // panel1
+            // richTextBox1
             // 
-            this.panel1.AutoScroll = true;
-            this.panel1.Location = new System.Drawing.Point(6, 19);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(432, 315);
-            this.panel1.TabIndex = 0;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(422, 314);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // button1
             // 
@@ -76,14 +80,57 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 19);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Do it!";
+            this.button1.Text = "Enter!";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(486, 64);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(504, 340);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Do it";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(281, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Sort AVG";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(362, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 20);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Sort MDM";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 416);
+            this.ClientSize = new System.Drawing.Size(1002, 416);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
@@ -101,8 +148,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Steam Game Selection/Form1.cs
+++ Steam Game Selection/Form1.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Steam_Game_Selection
 {
@@ -16,10 +17,22 @@ namespace Steam_Game_Selection
 
     public partial class Form1 : Form
     {
-        String[] game = new String[1000];
-        float[] avg = new float[1000];
-        float[] mdm = new float[1000];
-        String[] appid = new String[1000];
+        string[] mass = new string[140000];
+        String[] game = new String[50000];
+        String[] avg = new String[50000];
+        String[] mdm = new String[50000];
+        String[] appid = new String[50000];
+
+        private void OpenUri(string uri)
+        {
+            webBrowser1.Navigate(uri);
+
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+
+        }
 
         public Form1()
         {
@@ -28,7 +41,135 @@ namespace Steam_Game_Selection
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
-            
+            webBrowser1.Navigate("https://steamdb.info/calculator/"+ textBox1.Text +"/");
+
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            string htmlCod = "";
+
+            richTextBox1.Clear();
+            Array.Clear(mass, 0, 140000);
+            Array.Clear(game, 0, 50000);
+            Array.Clear(avg, 0, 50000);
+            Array.Clear(mdm, 0, 50000);
+            Array.Clear(appid, 0, 50000);
+
+            int j = 0;
+
+            htmlCod = webBrowser1.DocumentText;
+
+            for (int i = 0; mass[j] != "</html>"; i++)
+            {
+                if (htmlCod[i] != '\n')
+                {
+                    mass[j] = mass[j] + htmlCod[i];
+
+                }
+                else j++;
+
+            }
+
+            for (int i = 0; i <= j; i++)
+            {
+                if (mass[i] == null) mass[i] = " ";
+
+            }
+
+            int z = 0;
+
+            Regex regex = new Regex(@"<tr class=""app"" data-appid=""([0-9]+)");
+
+            for (int i = 493; i <= j; i++)
+            {
+                MatchCollection matches = regex.Matches(mass[i]);
+
+                foreach (Match match in matches)
+                {
+                    appid[z] = match.Groups[1].Value;
+                    z++;
+
+                }
+
+            }
+
+            for (int y = 0; y < z; y++)
+            {
+                OpenUri("https://steamdb.info/app/" + appid[y] + "/");
+
+                htmlCod = webBrowser1.DocumentText;
+                Array.Clear(mass,0,140000);
+
+                j = 0;
+
+                for (int i = 0; mass[j] != "</html>"; i++)
+                {
+                    if (htmlCod[i] != '\n')
+                    {
+                        mass[j] = mass[j] + htmlCod[i];
+
+                    }
+                    else j++;
+
+                }
+
+                for (int i = 0; i <= j; i++)
+                {
+                    if (mass[i] == null) mass[i] = " ";
+
+                }
+
+                regex = new Regex(@"<meta property=""og:title"" content=""([A-Z,0-9,:,', ,a-z,-]+)");
+
+                MatchCollection matches = regex.Matches(mass[17]);
+                
+                foreach (Match match in matches)
+                {
+                    game[y] = match.Groups[1].Value;
+
+                }
+
+                int k = 0;
+
+                regex = new Regex(@"<strong class=""steamspy-stats-ml"">([0-9,.]+)");
+
+                for(int i = 0; i <= j; i++)
+                {
+                    if(mass[i].IndexOf("<strong class=\"steamspy-stats-ml\">") >= 0)
+                    {
+                        k = i;
+                        break;
+                    }
+
+                }
+
+                matches = regex.Matches(mass[k]);
+
+                foreach (Match match in matches)
+                {
+                    mdm[y] = match.Groups[1].Value;
+
+                } 
+
+                matches = regex.Matches(mass[k + 4]);
+
+                foreach (Match match in matches)
+                {
+                    avg[y] = match.Groups[1].Value;
+
+                }
+
+                richTextBox1.Text += (y+1) + " " + game[y] + " " + " " + avg[y] + " " + mdm[y] + "\n";
+
+            }
+
+         }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
         }
     }
 }
